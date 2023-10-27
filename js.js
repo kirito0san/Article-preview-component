a931d87f88af21de984f903aba0887054f377bd7
kirito0san <jodat.ahmedsyed@gmail.com>
2023-10-27 18:26:40 +0200

    lasto

--- js.js
+++ js.js
@@ -9,33 +9,10 @@ Share.addEventListener(`click`, () => {
         Share.children[0].style.filter = `brightness(0) invert(1)`
     } else {
         Share_span.style.display = `none`
-        Share.style.backgroundColor = `#ecf2f8`
-        Share.children[0].style.filter = `brightness(1) invert(0)`
+        Share.style.backgroundColor = ``
+        Share.children[0].style.filter = ``
     }
 })
 
-window.onload = () => {
-    res()
-}
-window.onresize = () => {
-    res()
-}
 
-function res() {
-    if (window.innerWidth < 752) {
-        one1.style.display = `flex`
-        one1.addEventListener(`click`, () => {
-            if (Share_span.style.display == `` || Share_span.style.display == `none`) {
-                Share_span.style.display = `flex`
-                Share.style.backgroundColor = `hsl(214, 17%, 51%)`
-                Share.children[0].style.filter = `brightness(0) invert(1)`
-            } else {
-                Share_span.style.display = `none`
-                Share.style.backgroundColor = `#ecf2f8`
-                Share.children[0].style.filter = `brightness(1) invert(0)`
-            }
-        })
-    } else {
-        one1.style.display = `none`
-    }
-}
+
